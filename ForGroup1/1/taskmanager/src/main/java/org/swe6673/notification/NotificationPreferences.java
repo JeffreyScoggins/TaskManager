a9f818b623e2ccd464f1a8f42cb5d173afdc969f
JeffreyScoggins <JeffreyScoggins84@gmail.com>
2024-03-24 13:17:02 -0400

    Update NotificationPreferences.java

--- ForGroup1/1/taskmanager/src/main/java/org/swe6673/notification/NotificationPreferences.java
+++ ForGroup1/1/taskmanager/src/main/java/org/swe6673/notification/NotificationPreferences.java
@@ -1,21 +1,42 @@
 package org.swe6673.notification;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class NotificationPreferences {
 
-    private String userEmail;
-    private boolean optInForEmail;
+    private List<String> users = new ArrayList<>();
+    private List<Boolean> optInForEmail = new ArrayList<>();
 
-    public NotificationPreferences(String userEmail) {
-        this.userEmail = userEmail;
-        this.optInForEmail = true;
+
+    public NotificationPreferences() {
+
+
     }
 
-    public void setOptInForEmail(boolean optInForEmail) {
-        this.optInForEmail = optInForEmail;
+    public void setNotificationPreferences(String userEmail, boolean optInForEmail){
+        this.users.add(userEmail);
+        this.optInForEmail.add(optInForEmail);
     }
 
-    public boolean isOptInForEmail() {
-        return this.optInForEmail;
+    public void setOptInForEmail(String userEmail, boolean optInForEmail) {
+        for (int i = 0; i < this.users.size(); i++) {
+            if (Objects.equals(this.users.get(i), userEmail)) {
+                System.out.println(this.optInForEmail.get(i));
+            }
+        }
     }
+
+    public boolean isOptInForEmail(String userEmail){
+        boolean optInForEmail = false;
+            for (int i = 0; i < this.users.size(); i++) {
+                if (Objects.equals(this.users.get(i), userEmail)) {
+                    optInForEmail = this.optInForEmail.get(i);
+                    break;
+                }
+            }
+            return optInForEmail;
+        }
 
 }
